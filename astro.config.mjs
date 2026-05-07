ef9b27c21370e6995435c71730a19e1f6b8d927a
Hillary Njuguna <thegeiya@gmail.com>
2026-05-07 14:00:54 +0800

    fix(build): add react integration for cir-assessment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://hillary-site.vercel.app',
   output: 'static',
   adapter: vercel(),
-  integrations: [sitemap()]
+  integrations: [sitemap(), react()]
 });
